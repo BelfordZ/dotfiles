e2e93b10f5787514c3392e9325e21ec906acde0d
BelfordZ <belfordz66@gmail.com>
2018-03-16 13:32:21 -0700

    removed silly stuff

--- aliases/lol.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-swear () {
-    arr=('dick-ramming son of a shit-snack' fuck shit ass titty nipples cock 'ass-hat' 'butt-snatch' 'crap-cakes')
-    say -i --voice="Karen" "${arr[$[$RANDOM % ${#arr[@]}]]}"
-}
-
-alias zzz="swear"
